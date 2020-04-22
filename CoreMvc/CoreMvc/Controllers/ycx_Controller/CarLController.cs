01b4a24d534305f191927ed145fb2e58fcf32826
yyy965497752 <yanchong6535@163.com>
2020-04-22 15:50:31 +0800

    ycx_创建项目

---
+++ CoreMvc/CoreMvc/Controllers/ycx_Controller/CarLController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvc.Controllers.ycx_Controller
+{
+    public class CarLController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
